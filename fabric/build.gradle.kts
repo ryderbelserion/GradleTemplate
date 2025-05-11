5694bbb1bb8528e9841fca4bda00e64c8a91de60
Ryder Belserion <contact@ryderbelserion.com>
2025-05-10 21:06:02 -0400

    Add fabric module

---
+++ fabric/build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    alias(libs.plugins.fabric.loom)
+
+    id("root-plugin")
+}
+
+project.group = "${rootProject.group}.fabric"
+project.version = rootProject.version
+project.description = "A version of GradleTemplate for Fabric based servers!"
+
+base {
+    archivesName = "${rootProject.name}-${name}"
+}
+
+repositories {
+    maven("https://maven.fabricmc.net/")
+}
+
+dependencies {
+    minecraft(libs.minecraft)
+    mappings(loom.officialMojangMappings())
+    modCompileOnly(libs.fabric.loader)
+    modCompileOnly(libs.fabric.api)
+}
+
+tasks {
+    processResources {
+        filteringCharset = Charsets.UTF_8.name()
+
+        filesMatching("fabric.mod.json") {
+            expand("name" to rootProject.name,
+                "id" to rootProject.name.lowercase(),
+                "description" to project.description,
+                "version" to project.version,
+                "minecraft" to libs.versions.minecraft.get(),
+                "fabricloader" to libs.versions.fabricLoader.get())
+        }
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,26 +1,10 @@
+pluginManagement {
+    repositories {
+        maven("https://maven.fabricmc.net/")
+        gradlePluginPortal()
+    }
+}
+
 rootProject.name = "GradleTemplate"
 
-listOf(
-    "paper" to "paper",
-    "publish" to "publish"
-).forEach(::includeProject)
-
-fun includeProject(pair: Pair<String, String>): Unit = includeProject(pair.first, pair.second)
-
-fun includeProject(name: String, block: ProjectDescriptor.() -> Unit) {
-    include(name)
-    project(":$name").apply(block)
-}
-
-fun includeProject(path: String, name: String) {
-    includeProject(name) {
-        this.name = "${rootProject.name.lowercase()}-$name"
-        this.projectDir = File(path)
-    }
-}
-
-fun includeProject(name: String) {
-    includeProject(name) {
-        this.name = "${rootProject.name.lowercase()}-$name"
-    }
-}
+include("paper", "fabric")
